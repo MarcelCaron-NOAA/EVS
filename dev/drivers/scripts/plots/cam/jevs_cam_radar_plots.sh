b30a57a5228c8fc7f7b48130913fe0a3823bc29a
marcel caron <marcel.caron@clogin04.cactus.wcoss2.ncep.noaa.gov>
2023-11-16 19:04:05 +0000

    export evs_ver_2d

--- dev/drivers/scripts/plots/cam/jevs_cam_radar_plots.sh
+++ dev/drivers/scripts/plots/cam/jevs_cam_radar_plots.sh
@@ -31,7 +31,8 @@ module reset
 module load prod_envir/${prod_envir_ver}
 
 source $HOMEevs/modulefiles/$COMPONENT/${COMPONENT}_${STEP}.sh
-
+export evs_ver=v1.0.0
+export evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
 
 ############################################################
 # For dev testing
