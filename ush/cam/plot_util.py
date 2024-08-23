Fix unit conversion bug

ush/cam/plot_util.py
@@ -836,22 +836,24 @@ def calculate_bootstrap_ci(logger, bs_method, model_data, stat, nrepl, level,
          oobar = model_data.loc[:]['OOBAR']
          if bool_convert:
              coef, const = conversion
-             fbar = coef*fbar+const
-             obar = coef*obar+const
+             fbar_og = fbar
+             obar_og = obar
+             fbar = coef*fbar_og+const
+             obar = coef*obar_og+const
              fobar = (
                 np.power(coef, 2)*fobar 
-                + coef*const*fbar 
-                + coef*const*obar
+                + coef*const*fbar_og 
+                + coef*const*obar_og
                 + np.power(const, 2)
              )
              ffbar = (
                 np.power(coef, 2)*ffbar 
-                + 2.*coef*const*fbar 
+                + 2.*coef*const*fbar_og 
                 + np.power(const, 2)
              )
              oobar = (
                 np.power(coef, 2)*oobar 
-                + 2.*coef*const*obar
+                + 2.*coef*const*obar_og
                 + np.power(const, 2)
              )
       elif all(elem in model_data_columns for elem in 
@@ -889,23 +891,27 @@ def calculate_bootstrap_ci(logger, bs_method, model_data, stat, nrepl, level,
          uvoobar = model_data.loc[:]['UVOOBAR']
          if bool_convert:
              coef, const = conversion
-             ufbar = coef*ufbar+const
-             vfbar = coef*vfbar+const
-             uobar = coef*uobar+const
-             vobar = coef*vobar+const
+             ufbar_og = ufbar
+             vfbar_og = vfbar
+             uobar_og = uobar
+             vobar_og = vobar
+             ufbar = coef*ufbar_og+const
+             vfbar = coef*vfbar_og+const
+             uobar = coef*uobar_og+const
+             vobar = coef*vobar_og+const
              uvfobar = (
                 np.power(coef, 2)*uvfobar 
-                + coef*const*(ufbar + uobar + vfbar + vobar) 
+                + coef*const*(ufbar_og + uobar_og + vfbar_og + vobar_og) 
                 + np.power(const, 2)
              )
              uvffbar = (
                 np.power(coef, 2)*uvffbar 
-                + 2.*coef*const*(ufbar + vfbar) 
+                + 2.*coef*const*(ufbar_og + vfbar_og) 
                 + np.power(const, 2)
              )
              uvoobar = (
                 np.power(coef, 2)*uvoobar 
-                + 2.*coef*const*(uobar + vobar) 
+                + 2.*coef*const*(uobar_og + vobar_og) 
                 + np.power(const, 2)
              )
       elif all(elem in model_data_columns for elem in 
@@ -1579,7 +1585,6 @@ def calculate_stat(logger, model_data, stat, conversion):
 
         Returns:
            stat_values       - Dataframe of the statistic values
-           stat_values_array - array of the statistic values
            stat_plot_name    - string of the formal statistic
                                name being plotted
    """
@@ -1609,22 +1614,24 @@ def calculate_stat(logger, model_data, stat, conversion):
          oobar = model_data.loc[:]['OOBAR']
          if bool_convert:
              coef, const = conversion
-             fbar = coef*fbar+const
-             obar = coef*obar+const
+             fbar_og = fbar
+             obar_og = obar
+             fbar = coef*fbar_og+const
+             obar = coef*obar_og+const
              fobar = (
                 np.power(coef, 2)*fobar 
-                + coef*const*fbar 
-                + coef*const*obar
+                + coef*const*fbar_og 
+                + coef*const*obar_og
                 + np.power(const, 2)
              )
              ffbar = (
                 np.power(coef, 2)*ffbar 
-                + 2.*coef*const*fbar 
+                + 2.*coef*const*fbar_og 
                 + np.power(const, 2)
              )
              oobar = (
                 np.power(coef, 2)*oobar 
-                + 2.*coef*const*obar
+                + 2.*coef*const*obar_og
                 + np.power(const, 2)
              )
       elif all(elem in model_data_columns for elem in 
@@ -1660,23 +1667,27 @@ def calculate_stat(logger, model_data, stat, conversion):
          uvoobar = model_data.loc[:]['UVOOBAR']
          if bool_convert:
              coef, const = conversion
-             ufbar = coef*ufbar+const
-             vfbar = coef*vfbar+const
-             uobar = coef*uobar+const
-             vobar = coef*vobar+const
+             ufbar_og = ufbar
+             vfbar_og = vfbar
+             uobar_og = uobar
+             vobar_og = vobar
+             ufbar = coef*ufbar_og+const
+             vfbar = coef*vfbar_og+const
+             uobar = coef*uobar_og+const
+             vobar = coef*vobar_og+const
              uvfobar = (
                 np.power(coef, 2)*uvfobar 
-                + coef*const*(ufbar + uobar + vfbar + vobar) 
+                + coef*const*(ufbar_og + uobar_og + vfbar_og + vobar_og) 
                 + np.power(const, 2)
              )
              uvffbar = (
                 np.power(coef, 2)*uvffbar 
-                + 2.*coef*const*(ufbar + vfbar) 
+                + 2.*coef*const*(ufbar_og + vfbar_og) 
                 + np.power(const, 2)
              )
              uvoobar = (
                 np.power(coef, 2)*uvoobar 
-                + 2.*coef*const*(uobar + vobar) 
+                + 2.*coef*const*(uobar_og + vobar_og) 
                 + np.power(const, 2)
              )
       elif all(elem in model_data_columns for elem in 
@@ -2036,96 +2047,7 @@ def calculate_stat(logger, model_data, stat, conversion):
       logger.error("FATAL ERROR: "+stat+" is not a valid option")
       exit(1)
    nindex = stat_values.index.nlevels
-   if stat == 'fbar_obar' or stat == 'orate_frate' or stat == 'baser_frate':
-      try:
-         if nindex == 1:
-            index0 = len(stat_values_fbar.index.get_level_values(0).unique())
-            stat_values_array_fbar = (
-               np.ma.masked_invalid(
-                  stat_values_fbar.values.reshape(index0)
-               )
-            )
-            index0 = len(stat_values_obar.index.get_level_values(0).unique())
-            stat_values_array_obar = (
-               np.ma.masked_invalid(
-                  stat_values_obar.values.reshape(index0)
-               )
-            )
-         elif nindex == 2:
-            index0 = len(stat_values_fbar.index.get_level_values(0).unique())
-            index1 = len(stat_values_fbar.index.get_level_values(1).unique())
-            stat_values_array_fbar = (
-               np.ma.masked_invalid(
-                  stat_values_fbar.values.reshape(index0, index1)
-               )
-            )
-            index0 = len(stat_values_obar.index.get_level_values(0).unique())
-            index1 = len(stat_values_obar.index.get_level_values(1).unique())
-            stat_values_array_obar = (
-               np.ma.masked_invalid(
-                  stat_values_obar.values.reshape(index0, index1)
-               )
-            )
-         elif nindex == 3:
-            index0 = len(stat_values_fbar.index.get_level_values(0).unique())
-            index1 = len(stat_values_fbar.index.get_level_values(1).unique())
-            index2 = len(stat_values_fbar.index.get_level_values(2).unique())
-            stat_values_array_fbar = (
-               np.ma.masked_invalid(
-                  stat_values_fbar.values.reshape(index0, index1, index2)
-               )
-            )
-            index0 = len(stat_values_obar.index.get_level_values(0).unique())
-            index1 = len(stat_values_obar.index.get_level_values(1).unique())
-            index2 = len(stat_values_obar.index.get_level_values(2).unique())
-            stat_values_array_obar = (
-               np.ma.masked_invalid(
-                  stat_values_obar.values.reshape(index0, index1, index2)
-               )
-            )
-         stat_values_array = np.ma.array([stat_values_array_fbar,
-                                          stat_values_array_obar])
-      except ValueError as e:
-         logger.warning(e)
-         logger.warning("This is usually OK, and will happen if "
-                        + "event_equalization=False.") 
-         logger.warning("Setting stat_values_array to Nonetype.")
-         stat_values_array = None
-         logger.warning("Continuing ...")
-   else:
-      try:
-         if nindex == 1:
-            index0 = len(stat_values.index.get_level_values(0).unique())
-            stat_values_array = (
-               np.ma.masked_invalid(
-                  stat_values.values.reshape(1, index0)
-               )
-            )
-         elif nindex == 2:
-            index0 = len(stat_values.index.get_level_values(0).unique())
-            index1 = len(stat_values.index.get_level_values(1).unique())
-            stat_values_array = (
-               np.ma.masked_invalid(
-                  stat_values.values.reshape(1, index0, index1)
-               )
-            )
-         elif nindex == 3:
-            index0 = len(stat_values.index.get_level_values(0).unique())
-            index1 = len(stat_values.index.get_level_values(1).unique())
-            index2 = len(stat_values.index.get_level_values(2).unique())
-            stat_values_array = (
-               np.ma.masked_invalid(
-                  stat_values.values.reshape(1, index0, index1, index2)
-               )
-            )
-      except ValueError as e:
-         logger.warning(e)
-         logger.warning("This is usually OK, and will happen if "
-                        + "event_equalization=False.") 
-         logger.warning("Setting stat_values_array to Nonetype.")
-         stat_values_array = None
-         logger.warning("Continuing ...")
-   return stat_values, stat_values_array, stat_plot_name
+   return stat_values, None, stat_plot_name
 
 def get_lead_avg_file(stat, input_filename, fcst_lead, output_base_dir):
    lead_avg_filename = stat + '_' + os.path.basename(input_filename) \
@@ -2346,3 +2268,12 @@ def get_MCTC_cols_for_sum(n_cats, i_vals, ctc_metric_name):
         sys.exit(1)
     return cols
 
+def get_model_stats_key(model_alias_dict, requested_model):
+    if requested_model not in model_alias_dict:
+        return requested_model
+    else:
+        stats_key = model_alias_dict[requested_model]['stats_key']
+        if not stats_key:
+            return requested_model
+        else:
+            return stats_key