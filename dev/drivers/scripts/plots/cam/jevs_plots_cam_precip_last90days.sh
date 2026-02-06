a23956ddd97055c6afbc52da2b1216e2d8d3fa5b
marcel caron <marcel.caron@dlogin03.dogwood.wcoss2.ncep.noaa.gov>
2026-02-06 19:36:53 +0000

    driver changes for personal runs

--- dev/drivers/scripts/plots/cam/jevs_plots_cam_precip_last90days.sh
+++ dev/drivers/scripts/plots/cam/jevs_plots_cam_precip_last90days.sh
@@ -34,7 +34,7 @@ export VERIF_CASE="precip"
 export MODELNAME=${COMPONENT}
 
 # EVS Settings
-export HOMEevs="/lfs/h2/emc/vpppg/noscrub/$USER/EVS"
+export HOMEevs="/lfs/h2/emc/vpppg/noscrub/$USER/test/arefs/EVS"
 export config=$HOMEevs/parm/evs_config/cam/config.evs.prod.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}
 
 # Load Modules
@@ -47,10 +47,10 @@ evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
 # Developer Settings
 export envir=prod
 export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
-export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d
+export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d/arafs
 export vhr=${vhr:-${vhr}}
-export COMOUT=/lfs/h2/emc/ptmp/$USER/$NET/$evs_ver_2d/$STEP/$COMPONENT
-export EVAL_PERIOD="last90days"
+export COMOUT=/lfs/h2/emc/ptmp/$USER/$NET/$evs_ver_2d/arafs/$STEP/$COMPONENT
+export EVAL_PERIOD="last31days"
 
 # Job Settings and Run
 . ${HOMEevs}/jobs/JEVS_PLOTS_CAM
--- dev/drivers/scripts/stats/cam/jevs_stats_cam_arefsmem_precip.sh
+++ dev/drivers/scripts/stats/cam/jevs_stats_cam_arefsmem_precip.sh
@@ -35,7 +35,7 @@ export job=${PBS_JOBNAME:-jevs_${STEP}_${COMPONENT}_${MODELNAME}_${VERIF_CASE}}
 export jobid=$job.${PBS_JOBID:-$$}
 
 # EVS Settings
-export HOMEevs="/lfs/h2/emc/vpppg/noscrub/$USER/EVS"
+export HOMEevs="/lfs/h2/emc/vpppg/noscrub/$USER/test/arefs/EVS"
 export HOMEevs=${HOMEevs:-${PACKAGEROOT}/evs.${evs_ver}}
 export config=$HOMEevs/parm/evs_config/cam/config.evs.prod.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}.arefsmem
 
@@ -49,8 +49,8 @@ evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
 # Developer Settings
 export envir=prod
 export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
-export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d
-export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d/$STEP/$COMPONENT
+export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d/arafs
+export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d/arafs/$STEP/$COMPONENT
 export vhr=${vhr:-${vhr}}
 export MAILTO="andrew.benjamin@noaa.gov,marcel.caron@noaa.gov"
 
